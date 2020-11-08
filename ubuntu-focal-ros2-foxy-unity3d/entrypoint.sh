cf693b4dd8882c36baad94c9ac6ee3588347369e
Valerio Magnago <valerio.magnago@hotmail.it>
2020-11-08 23:37:08 +0100

    chore: Removed outdated docker images

--- ubuntu-focal-ros2-foxy-unity3d/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup ros2 environment
-source "/opt/ros/foxy/setup.bash"
-#source "${DOTNET_WS}/install/local_setup.bash"
-
-exec "$@"
--- ubuntu-focal-ros2-foxy/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup ros2 environment
-. "${ROS2_WS}/install/local_setup.bash"
-
-exec "$@"
