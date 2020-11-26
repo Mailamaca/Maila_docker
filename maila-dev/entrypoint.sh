a590897c67212beb425a6790b1451827559734be
Valerio Magnago <valerio.magnago@hotmail.it>
2020-11-27 00:07:25 +0100

    chore: Reorganize the Dockerfile
    New base image to support ignition gazebo and reorganization of
    the commands

--- maila-dev/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup ros2 environment
-source "/opt/ros/$ROS_DISTRO/setup.bash"
-echo "source /home/snail/Workspace/Maila/install/setup.sh" > /home/snail/.bashrc
-exec "$@"
